0330f38234393a6ae8cb7bb289515fd4c1dc0501
Duke, Jesse <jesse.duke@gmail.com>
2023-04-17 21:12:25 -0400

    Adding functions for emailer

--- functions/index.js
+++ functions/index.js
@@ -1,50 +1,29 @@
-// Import the Firebase SDK for Google Cloud Functions.
+// Imports
 const functions = require('firebase-functions');
-const { Timestamp } = require ('firebase/firestore');
-
-// Import and initialize the Firebase Admin SDK.
 const admin = require('firebase-admin');
+
+// Initialize the Firebase Admin SDK.
 admin.initializeApp();
 
+exports.sendEMailNotification = functions.firestore
+            .document('/email-inquiry/{documentID}').onCreate(async (snap, context) => {
+    
+            let emailInquiry = snap.data();
 
-// // TODO(DEVELOPER): Write the addWelcomeMessages Function here.
-// exports.addWelcomeMessages = functions.auth.user().onCreate(async (user) => {
-//     functions.logger.log('A new user signed in for the first time.');
-//     const fullName = user.displayName || 'Anonymous';
-
-//     // Saves the new welcome message into the database
-//     // which then displays it in the FriendlyChat clients.
-//     let updated_at_timestamp = Timestamp.now().toDate()
-//     console.log ("CurrTime", updated_at_timestamp)
-//     await admin.firestore().collection('messages').add({
-//       name: 'Firebase Bot',
-//       profilePicUrl: '/images/firebase-logo.png', // Firebase logo
-//       text: `${fullName} signed in for the first time! Welcome!`,
-//       timestamp: updated_at_timestamp
-//     });
-//     functions.logger.log('Welcome message written to database.');
-//   });
-
-// TEST FOR MAIL
-exports.sendWelcomeEmail = admin.firestore().collection("email-inquiry").onCreate(async (snapshot) => {
-
-  // Get data from entry
-  const textData = snapshot.data().textData;
-  console.log("Text DATA:",textData)
-
-  // Write to collection as requested
-  // await admin.firestore().collection("mailer-log").add({
-  //     to: user.email,
-  //     message: {
-  //       subject: "Welcome!",
-  //       html: `
-  //         Hey ${user.displayName || ""}! 👋
-
-  //         Thank you for signing up.
-  //       `,
-  //     },
-  //   });
+            // Write to collection as requested
+            await admin.firestore().collection("mail-log").add({
+                to: `${process.env.SEND_TO}`,
+                message: {
+                subject: `DORA.dev Email Inquiry ${emailInquiry.inquiry_type}`,
+                html: `
+                    <br>From: ${emailInquiry.from_email || ""}
+                    <br>First Name: ${emailInquiry.first_name || ""}
+                    <br>Last Name: ${emailInquiry.last_name || ""}
+                    <br>Submitted:  ${context.timestamp || ""}
+                    <hr>
+                    <br>Message: ${emailInquiry.message || ""}
+    
+                `
+                },
+            });
 });
-
-
-// TODO(DEVELOPER): Write the sendNotifications Function here.
