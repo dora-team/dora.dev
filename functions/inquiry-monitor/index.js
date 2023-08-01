5407023d942d4ea7dc229a0606f4087f27cd64e0
Jesse <jesseduke@google.com>
2023-08-01 12:51:07 -0400

    Initial Testing via emulator

--- functions/inquiry-monitor/index.js
+++ functions/inquiry-monitor/index.js
@@ -10,6 +10,7 @@
  */
 const functions = require('firebase-functions');
 var admin = require('firebase-admin');
+
 admin.initializeApp();
 const remoteConfig = admin.remoteConfig();
 
@@ -56,17 +57,8 @@ exports.emailInquiryMonitor = functions.firestore
             // Write to collection as requested according to the plugin
             await admin.firestore().collection(MONITOR_EXTENSION_COLLECTION).add({
                 to: sendTo,
-                message: {
-                subject: `DORA.dev inquiry from ${emailInquiry.first_name || ""} ${emailInquiry.last_name || ""}: ${emailInquiry.inquiry_type || ""}`,
-                html: `
-                    <br>From: ${emailInquiry.from_email || ""}
-                    <br>First Name: ${emailInquiry.first_name || ""}
-                    <br>Last Name: ${emailInquiry.last_name || ""}
-                    <br>Submitted:  ${context.timestamp || ""}
-                    <hr>
-                    <br>Message: ${emailInquiry.message || "Check /email-inquiry/{documentID} for valid entry."}
-                `
-                },
+                message: {subject: `DORA.dev inquiry from ${emailInquiry.first_name || ""} ${emailInquiry.last_name || ""}: ${emailInquiry.inquiry_type || ""}`,html: `<br>From: ${emailInquiry.from_email || ""}<br>First Name: ${emailInquiry.first_name || ""}<br>Last Name: ${emailInquiry.last_name || ""}<br>Submitted:  ${context.timestamp || ""}<hr><br>Message: ${emailInquiry.message || "Check /email-inquiry/{documentID} for valid entry."}`
+            },
             });
 });
 
@@ -78,22 +70,19 @@ exports.emailInquiryMonitor = functions.firestore
  */
 async function getSendToEmailInquiry() {
 
+    if (process.env.MONITOR_SEND_TO) {
+        sendTo = process.env.MONITOR_SEND_TO;
+        return;
+    }
+
     try {
+
         let template = await remoteConfig.getTemplate();
         sendTo = template.parameters.INQUIRY_SEND_TO.defaultValue.value;
 
     } catch (error) {
         //log error and look for env variable
-        console.error(error.message);
-        console.error('ERROR:  Unable to get template from Remote Config. Looking for local environment variable.');
-        sendTo = process.env.SEND_TO;
-    }
-
-    // If falsy, log error and set DEFAULT_SEND_TO
-    if (!sendTo) {
-        console.error('ERROR:  Unable to identify INQUIRY_SEND_TO value, setting default');
+        console.warn('ERROR:  Unable to get template from Remote Config. Looking for local environment variable.');
         sendTo = DEFAULT_SEND_TO
     }
-
 }
-
