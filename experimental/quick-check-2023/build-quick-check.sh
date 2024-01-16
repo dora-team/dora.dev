a421d0854a9ff9a0cce2038cbd9024638dc0bf3c
Dave Stanke <davidstanke@gmail.com>
2024-01-16 13:53:58 -0500

    add CI

--- experimental/quick-check-2023/build-quick-check.sh
+++ experimental/quick-check-2023/build-quick-check.sh
@@ -3,7 +3,8 @@
 # run this script from the project root
 
 # build production assets
-npm --prefix ./experimental/quick-check-2023/ run build;
+npm --prefix ./experimental/quick-check-2023 install
+npm --prefix ./experimental/quick-check-2023/ run build
 
 # copy assets to the quickcheck content folder
 cp ./experimental/quick-check-2023/dist/assets/index-*.js ./hugo/content/quickcheck/2023/quickcheck.js
