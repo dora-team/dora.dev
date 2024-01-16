f149bf8323ad81963623d1e75f70cb33b6f6c29c
Dave Stanke <davidstanke@gmail.com>
2024-01-16 13:25:05 -0500

    fix paths

--- experimental/quick-check-2023/build-quick-check.sh
+++ experimental/quick-check-2023/build-quick-check.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 
+# run this script from the project root
+
 # build production assets
-# npm run build;
+npm --prefix ./experimental/quick-check-2023/ run build;
 
 # copy assets to the quickcheck content folder
-cp dist/assets/index-*.js ../../hugo/content/quickcheck/2023/quickcheck.js
-cp dist/assets/index-*.css ../../hugo/content/quickcheck/2023/quickcheck.css
+cp ./experimental/quick-check-2023/dist/assets/index-*.js ./hugo/content/quickcheck/2023/quickcheck.js
+cp ./experimental/quick-check-2023/dist/assets/index-*.css ./hugo/content/quickcheck/2023/quickcheck.css
