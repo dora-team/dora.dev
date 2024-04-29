f0b3e12db78c220e5573b1e0a570c7be1d1c39a5
Dave Stanke <davidstanke@gmail.com>
2024-04-29 11:09:36 -0400

    disable safari for performance (and it's not really needed)

--- test/playwright/playwright.config.ts
+++ test/playwright/playwright.config.ts
@@ -56,10 +56,10 @@ export default defineConfig({
     //   use: { ...devices['Pixel 5'] },
     // },
 
-    {
-      name: 'Mobile Safari',
-      use: { ...devices['iPhone 12'] },
-    },
+    // {
+    //   name: 'Mobile Safari',
+    //   use: { ...devices['iPhone 12'] },
+    // },
 
     /* Test against branded browsers. */
     // {
