c11208df462b3b97c2c189c0d054b4a0baec9f31
Nathen Harvey <nathenharvey@google.com>
2024-06-06 16:56:24 -0400

    Add tests for survey and 2024 research
    Signed-off-by: Nathen Harvey <nathenharvey@google.com>

--- test/playwright/tests/survey.spec.ts
+++ test/playwright/tests/survey.spec.ts
@@ -1,15 +1,16 @@
 const { test, expect } = require('@playwright/test');
 
 test('Verify the page title', async ({ page }) => {
-  await page.goto('/research/2024/');
-  await expect(page).toHaveTitle('DORA | DORA Research: 2024');
+  await page.goto('/survey/');
+  await expect(page).toHaveTitle('DORA | The 2024 DORA Survey');
 });
 
 test('Survey Image', async ({ page }) => {
-  await page.goto('/research/2024/');
+  await page.goto('/survey/');
   await page.waitForLoadState("domcontentloaded");
   const surveyImage = await page.locator('img[alt="2024 DORA Survey - Shape the future of tech"]');
   const imgSrc = await surveyImage.getAttribute('src');
-  const res = await page.request.get(imgSrc);
+  const res = await page.request.get('/survey/' + imgSrc);
   expect(res.status()).toBe(200);
 });
+
