c2e25c01d31f5782364b2551e2bf00718809a1c8
Nathen Harvey <nathenharvey@google.com>
2024-07-24 16:57:00 -0400

    Initial playwright tests for the publications page
    We're going to move around some files so let's have some tests in place before we do so.
    
    Signed-off-by: Nathen Harvey <nathenharvey@google.com>

---
+++ test/playwright/tests/publications/index.spec.ts
@@ -0,0 +1,43 @@
+import { test, expect } from '@playwright/test';
+
+test('Publications page loads correctly', async ({ page }) => {
+  await page.goto('/publications/');
+
+  // Check for page title
+  await expect(page).toHaveTitle('DORA | DORA Publications');
+
+  // Check for page heading
+  await expect(page.locator('h1')).toContainText('Publications by DORA');
+
+  // Click the "read the report" link and expect to be directed to https://cloud.google.com/devops/state-of-devops in a new tab
+  const [newPage] = await Promise.all([
+    page.waitForEvent('popup'),
+    page.getByRole('link', { name: 'Read the report' }).click(),
+  ]);
+
+  await expect(newPage).toHaveURL('https://cloud.google.com/devops/state-of-devops');
+});
+
+// test('test', async ({ page }) => {
+//   await page.goto('/publications/');
+//   await page.getByRole('link', { name: 'Download the report' }).first().click();
+//   const page1Promise = page.waitForEvent('popup');
+//   await page.getByRole('link', { name: 'English' }).click();
+//   const page1 = await page1Promise;
+//   await page.locator('body').press('ControlOrMeta+ArrowLeft');
+//   await page.getByRole('link', { name: '2022', exact: true }).click();
+//   await page.getByRole('link', { name: 'DORA Report' }).click();
+//   const page2Promise = page.waitForEvent('popup');
+//   await page.getByRole('link', { name: 'English' }).click();
+//   const page2 = await page2Promise;
+//   await page.getByRole('link', { name: '2021' }).click();
+//   await page.getByRole('link', { name: 'DORA Report' }).click();
+//   const page3Promise = page.waitForEvent('popup');
+//   await page.getByRole('link', { name: 'English' }).click();
+//   const page3 = await page3Promise;
+//   await page.getByRole('link', { name: '2019' }).click();
+//   await page.getByRole('link', { name: 'DORA Report' }).click();
+//   const page4Promise = page.waitForEvent('popup');
+//   await page.getByRole('link', { name: 'Accelerate State of DevOps Report' }).click();
+//   const page4 = await page4Promise;
+// });
