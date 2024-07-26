2158004db041bd670a80a5e9965b466986190619
Nathen Harvey <nathenharvey@google.com>
2024-07-25 21:16:35 -0400

    Add tests for 2022 report page
    Signed-off-by: Nathen Harvey <nathenharvey@google.com>

---
+++ test/playwright/tests/research/2021/dora-report.spec.ts
@@ -0,0 +1,48 @@
+import { test, expect } from '@playwright/test';
+
+const sidebarLinks = [
+  'Meet DORA\'s Research Team',
+  '2024',
+  '2023',
+  '2022',
+  '2021',
+  '2020',
+  '2019',
+  '2018',
+  '2017 and earlier'
+];
+
+const languageToUrlMap = {
+  'English': '2021-dora-accelerate-state-of-devops-report.pdf',
+  'Français': '2021-dora-accelerate-state-of-devops-report-fr.pdf',
+  'Italiano': '2021-dora-accelerate-state-of-devops-report-it.pdf',
+  'Polski': '2021-dora-accelerate-state-of-devops-report-pl.pdf',
+  '繁體中文': '2021-dora-accelerate-state-of-devops-report-zh-tw.pdf',
+  '日本語': '2021-dora-accelerate-state-of-devops-report-ja.pdf',
+  '한국어': '2021-dora-accelerate-state-of-devops-report-ko.pdf'
+};
+
+test('2021 Report page loads correctly', async ({ page }) => {
+  await page.goto('/research/2021/dora-report/');
+
+  // Check for page title
+  await expect(page).toHaveTitle('DORA | Accelerate State of DevOps Report 2021');
+
+  // Check for page heading
+  await expect(page.locator('h1')).toContainText('DORA Research: 2021');
+
+  // Check languages
+  const languageOptions = await page.locator('item ul li').count();
+  await expect(languageOptions).toBe(7);
+
+  for (const language in languageToUrlMap) {
+    const url = languageToUrlMap[language];
+    const languageLink = page.getByRole('link', { name: language, exact: true });
+    await expect(languageLink).toHaveAttribute('href', url);
+  }
+
+  // Check the sidebar
+  for (const sidebarLink of sidebarLinks) {
+    await expect(page.getByRole('link', { name: sidebarLink, exact: true })).toBeVisible();
+  }
+});
