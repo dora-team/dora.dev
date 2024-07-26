ac70616765b46116789ae7938a9768687563abbc
Nathen Harvey <nathenharvey@google.com>
2024-07-25 21:33:21 -0400

    Refactor the 2024 research overview tests
    These now follow the same conventions as other research tests
    
    Signed-off-by: Nathen Harvey <nathenharvey@google.com>

--- test/playwright/tests/research/2024/overview.spec.ts
+++ test/playwright/tests/research/2024/overview.spec.ts
@@ -1,11 +1,28 @@
-const { test, expect } = require('@playwright/test');
+import { test, expect } from '@playwright/test';
 
-test('Verify the page title', async ({ page }) => {
+const sidebarLinks = [
+  'Meet DORA\'s Research Team',
+  '2024',
+  '2023',
+  '2022',
+  '2021',
+  '2020',
+  '2019',
+  '2018',
+  '2017 and earlier'
+];
+
+test('2024 research overive page loads correctly', async ({ page }) => {
   await page.goto('/research/2024/');
+
+  // Check for page title
   await expect(page).toHaveTitle('DORA | DORA Research: 2024');
-});
 
-test('The 2024 research page heading', async ({ page }) => {
-  await page.goto('/research/2024/');
+  // Check for page heading
   await expect(page.locator('h1')).toContainText('DORA Research: 2024');
+
+  // Check the sidebar
+  for (const sidebarLink of sidebarLinks) {
+    await expect(page.getByRole('link', { name: sidebarLink, exact: true })).toBeVisible();
+  }
 });
