9cfb5bddf8294912d0d4077e8a3b3eb7a58a2c59
Nathen Harvey <nathenharvey@google.com>
2024-07-30 22:36:49 -0400

    Add tests for each report on `/publications`
    Signed-off-by: Nathen Harvey <nathenharvey@google.com>

--- test/playwright/tests/publications/index.spec.ts
+++ test/playwright/tests/publications/index.spec.ts
@@ -30,9 +30,39 @@ test('Publications page links to the DORA awards ebook.', async ({ page }) => {
   )
 });
 
-test('Publications page links to the latest DORA Report', async ({ page }) => {
-  await expect(page.getByRole('link', { name: 'Read the Report' })).toHaveAttribute(
-    'href',
-    'https://cloud.google.com/devops/state-of-devops'
-  )
+// Mapping for "Download the report" links which, by convention, got to /research/:year or /research/:year/dora-report
+// TODO - make this an array of maps and generate an isolated test for each
+export const downloadTheReportMap = {
+  '0': '/research/2023/dora-report/',
+  '1': '/research/2022/dora-report/',
+  '2': '/research/2021/dora-report/',
+  '3': '/researc/2016/',
+  '4': '/research/2015/',
+  '5': '/research/2014/'
+}
+
+test('Publications page links to the DORA Report landing pages', async ({ page }) => {
+  for (const report in downloadTheReportMap) {
+    const url = downloadTheReportMap[report];
+    const reportLink = page.getByRole('link', { name: 'Download the report' }).nth(report)
+    await expect(reportLink).toHaveAttribute('href', url);
+    await expect(reportLink).toBeVisible();
+  }
+});
+
+// Mapping for the "Read PDF" links which link directly to the report
+// TODO - #698 calls for a landing page for each report. These should all move to the downloadTheReportMap
+export const readPDFMap = {
+  '0': '/research/2019/dora-report/2019-dora-accelerate-state-of-devops-report.pdf',
+  '1': '/research/2018/dora-report/2018-dora-accelerate-state-of-devops-report.pdf',
+  '2': '/research/2017-and-earlier/2017-state-of-devops-report.pdf'
+}
+
+test('Publications page links directly to DORA Reports', async ({ page }) => {
+  for (const report in readPDFMap) {
+    const url = readPDFMap[report];
+    const reportLink = page.getByRole('link', { name: 'Read PDF' }).nth(report)
+    await expect(reportLink).toHaveAttribute('href', url);
+    await expect(reportLink).toBeVisible();
+  }
 });
