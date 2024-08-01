1f4968f7e5bc24d4de2088e1ab65d977544c9221
Nathen Harvey <nathenharvey@google.com>
2024-08-01 09:30:23 -0400

    Update tests for `/resources`
    Signed-off-by: Nathen Harvey <nathenharvey@google.com>

--- test/playwright/tests/resources/resources.spec.ts
+++ test/playwright/tests/resources/resources.spec.ts
@@ -1,14 +1,38 @@
 import { test, expect } from '@playwright/test';
 
-test('test resources page', async ({ page }) => {
+export const toolsMap = {
+  'Apache DevLake™': 'https://devlake.apache.org/',
+  'Middleware': 'https://github.com/middlewarehq/middleware',
+  'OpenDORA': 'https://github.com/DevoteamNL/opendora'
+};
+
+test.beforeEach(async ({ page }) => {
   await page.goto('/resources');
+});
 
-  // Check for the source-available tools section
+test('Resources page has the correct title.', async ({ page }) => {
+  await expect(page).toHaveTitle('DORA | Resources');
+});
+
+test('Resources page has the correct header.', async ({ page }) => {
+  await expect(page.locator('h1')).toContainText('Resources');
+});
+
+test('Resources page includes source-available tools.', async ({ page }) => {
   await expect(page.getByText('Source-available tools')).toBeVisible();
+  for (const tool in toolsMap) {
+    const url = toolsMap[tool];
+    await expect(page.getByRole('link', { name: tool, exact: true })).toHaveAttribute(
+      'href',
+      url
+    );
+  }
+});
 
-  // Check for the books section
+test('Resources page includes books.', async ({ page }) => {
   await expect(page.getByText('Books')).toBeVisible();
+});
 
-  // Check for the graphics section
+test('Resources page includes graphics.', async ({ page }) => {
   await expect(page.getByText('Graphics')).toBeVisible();
 });
