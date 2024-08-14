7676cf926fb6b59d9e9dffdb48e9902ddaf312cb
Nathen Harvey <nathenharvey@google.com>
2024-08-13 21:16:44 -0400

    Add tests for the research team page
    Signed-off-by: Nathen Harvey <nathenharvey@google.com>

---
+++ test/playwright/tests/research/team.spec.ts
@@ -0,0 +1,22 @@
+import { test, expect } from '@playwright/test';
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('/research/team/');
+});
+
+test('Research Team page has the correct title.', async ({ page }) => {
+  await expect(page).toHaveTitle('DORA | Research Team');
+});
+
+test('Research Team page has the correct heading', async ({ page }) => {
+  await expect(page.locator('h1')).toContainText('Meet DORA’s Research Team');
+});
+
+test('Research Team page has correct number of researchers', async ({ page }) => {
+  const sizeOfResearchTeam = await page.locator('h3').count();
+  await expect(sizeOfResearchTeam).toBe(3);
+});
+
+test('Research Team page lists the DORA Collective', async ({ page }) => {
+  await expect(page.locator('h2')).toContainText('Meet the DORA Collective');
+});
