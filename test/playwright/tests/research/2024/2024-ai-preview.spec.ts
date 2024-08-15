b3a9b9dd3bac77ffdcd7e209455c6e4a36edadf0
Nathen Harvey <nathenharvey@google.com>
2024-08-15 10:43:02 -0400

    Fix names of tests for the ai preview page
    Signed-off-by: Nathen Harvey <nathenharvey@google.com>

--- test/playwright/tests/research/2024/2024-ai-preview.spec.ts
+++ test/playwright/tests/research/2024/2024-ai-preview.spec.ts
@@ -5,15 +5,15 @@ test.beforeEach(async ({ page }) => {
   await page.goto('/research/2024/ai-preview');
 });
 
-test('2024 research overview has the correct title.', async ({ page }) => {
+test('2024 AI preview has the correct title.', async ({ page }) => {
   await expect(page).toHaveTitle('DORA | 2024 DORA Report Preview');
 });
 
-test('2024 research overview has the correct header.', async ({ page }) => {
+test('2024 AI preview has the correct header.', async ({ page }) => {
   await expect(page.locator('h2')).toContainText('AI in the workplace: Adoption and impact');
 });
 
-test('2024 research overview has the correct sidebar.', async ({ page }) => {
+test('2024 AI preview has the correct sidebar.', async ({ page }) => {
   for (const sidebarLink of sidebarLinks) {
     await expect(page.getByRole('link', { name: sidebarLink, exact: true })).toBeVisible();
   }
