33f9679031ccebd70b5d0a3160b98980b8e2aac6
davidstanke <davidstanke@google.com>
2024-09-10 17:27:27 -0400

    remove firebase config

--- ci/firebase-config.doradotdev-staging.js
+++
@@ -1,9 +0,0 @@
-export const config = {
-    apiKey: "AIzaSyATzQBPDMDXsXdqzJLcQ5msW0qrrz-YP_Q",
-    authDomain: "doradotdev-staging.firebaseapp.com",
-    projectId: "doradotdev-staging",
-    storageBucket: "doradotdev-staging.appspot.com",
-    messagingSenderId: "280168604467",
-    appId: "1:280168604467:web:4cb9a9d802c56290e1d446",
-    measurementId: "G-F38S41VXVN"
-};
