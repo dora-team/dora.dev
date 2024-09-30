1aba545fb6c922841a714019b2948e7899aafafe
davidstanke <davidstanke@google.com>
2024-09-30 18:06:01 -0400

    Add Core v2 download as PDF  (Fixes #773)

--- svelte/core-v2/src/lib/Footer.svelte
+++ svelte/core-v2/src/lib/Footer.svelte
@@ -2,16 +2,28 @@
     export let view_mode = "summary";
     import png_url_summary from '../assets/png/dora-core-v2.0.0-summary.png'
     import png_url_detail from '../assets/png/dora-core-v2.0.0-detail.png'
+    import pdf_url_summary from '../assets/pdf/dora-core-v2.0.0-summary.pdf'
+    import pdf_url_detail from '../assets/pdf/dora-core-v2.0.0-detail.pdf'
 
-    let png_urls = {
+    let download_urls = {
+        "png": {
         "summary": png_url_summary,
         "detail": png_url_detail
+        },
+        "pdf": {
+        "summary": pdf_url_summary,
+        "detail": pdf_url_detail
+        },
     }
 </script>
 
 <div class="core-footer">
     <div class="version">DORA Core model v2.0.0</div>
-    <div><a href="{png_urls[view_mode]}" target="_blank">download as PNG</a></div>
+    <div>
+        download: 
+        <a href="{download_urls['png'][view_mode]}" target="_blank">PNG</a>
+        <a href="{download_urls['pdf'][view_mode]}" target="_blank">PDF</a>
+    </div>
 </div>
 
 <style>
@@ -21,13 +33,12 @@
         margin-bottom: 2rem;
         font-size: 0.75rem;
         column-gap: 1rem;
-    }
-
-    .version {
-        color: var(--color-grey-medium);
+        color:var(--color-grey-medium)
     }
 
     a {
-        color:var(--color-grey-medium)
+        color:var(--color-grey-medium);
+        display:inline-block;
+        margin-inline: .15rem;
     }
 </style>
