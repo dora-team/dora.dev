32637c463f687f23b56e67611c6da24ac425b631
Nathen Harvey <nathenharvey@google.com>
2024-10-04 14:41:02 -0400

    Add playwright tests for the sponsor pages.
    Signed-off-by: Nathen Harvey <nathenharvey@google.com>

---
+++ test/playwright/tests/sponsors/dora-brand-guidelines.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test';
+
+// baseURL default is defined in playwright.config.ts
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('/sponsors/dora-brand-guidelines/');
+});
+
+test('DORA brand guidelines has the correct title.', async ({ page }) => {
+  await expect(page).toHaveTitle('DORA | DORA Brand Guidelines');
+});
+
+test('DORA brand guidelines has the correct header.', async ({ page }) => {
+  await expect(page.locator('h1')).toContainText('DORA Brand Guidelines');
+});
