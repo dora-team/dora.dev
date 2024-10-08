66d4bb7df6082768b837e6edcde364fb14497ef0
Nathen Harvey <nathenharvey@google.com>
2024-10-07 22:38:38 -0400

    Add tests for questions pages
    Signed-off-by: Nathen Harvey <nathenharvey@google.com>

---
+++ test/playwright/tests/research/core/core-questions.spec.ts
@@ -0,0 +1,24 @@
+import { test, expect } from '@playwright/test';
+import { sidebarLinks } from '../sidebarLinks';
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('/research/core/questions/');
+});
+
+test('core questions page has the correct title.', async ({ page }) => {
+  await expect(page).toHaveTitle('DORA | DORA Research Questions');
+});
+
+test('core questions page has the correct header.', async ({ page }) => {
+  await expect(page.locator('h1')).toContainText('DORA Research: core');
+});
+
+test('core questions page lists the correct report.', async ({ page }) => {
+    await expect(page.locator('h4')).toContainText('Responses to the following questions were used in the analysis of the DORA Core Model.');
+});
+
+test('core questions page has the correct sidebar.', async ({ page }) => {
+  for (const sidebarLink of sidebarLinks) {
+    await expect(page.getByRole('link', { name: sidebarLink, exact: true })).toBeVisible();
+  }
+});
