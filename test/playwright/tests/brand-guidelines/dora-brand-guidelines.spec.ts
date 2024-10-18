081a319c0de15731cf4980341cd23e3d1476319d
Nathen Harvey <nathenharvey@google.com>
2024-10-18 19:38:48 -0400

    Add 2024 report cover art to the brand guidelines
    Signed-off-by: Nathen Harvey <nathenharvey@google.com>

--- test/playwright/tests/brand-guidelines/dora-brand-guidelines.spec.ts
+++ test/playwright/tests/brand-guidelines/dora-brand-guidelines.spec.ts
@@ -2,6 +2,12 @@ import { test, expect } from '@playwright/test';
 
 // baseURL default is defined in playwright.config.ts
 
+export const pageHeaders = [
+  'Branding Guidelines',
+  'Graphics',
+  '2024 DORA Report Graphics'
+];
+
 test.beforeEach(async ({ page }) => {
   await page.goto('/brand-guidelines/');
 });
@@ -12,4 +18,10 @@ test('DORA brand guidelines has the correct title.', async ({ page }) => {
 
 test('DORA brand guidelines has the correct header.', async ({ page }) => {
   await expect(page.locator('h1')).toContainText('DORA Brand Guidelines');
+});
+
+pageHeaders.forEach((pageHeader, index) => {
+  test(`DORA brand guidelines has a ${pageHeader} header.`, async ({ page }) => {
+    await expect(page.locator('h2').nth(index)).toContainText(pageHeader);
+  });
 });
