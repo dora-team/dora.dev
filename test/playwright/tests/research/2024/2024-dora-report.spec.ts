a1e68eff5964a6f28ee26ba17faf95c4ece3e6fd
Nathen Harvey <nathenharvey@google.com>
2024-10-18 16:26:11 -0400

    Remove test for languages of the 2024 report
    There is only English for now
    
    Signed-off-by: Nathen Harvey <nathenharvey@google.com>

--- test/playwright/tests/research/2024/2024-dora-report.spec.ts
+++ test/playwright/tests/research/2024/2024-dora-report.spec.ts
@@ -1,10 +1,6 @@
 import { test, expect } from '@playwright/test';
 import { sidebarLinks } from '../sidebarLinks';
 
-const languageToUrlMap = {
-  'English': 'https://cloud.google.com/devops/state-of-devops/?hl=en&region=US'
-};
-
 test.beforeEach(async ({ page }) => {
   await page.goto('/research/2024/dora-report/');
 });
@@ -15,17 +11,6 @@ test('2024 DORA report page has the correct title.', async ({ page }) => {
 
 test('2024 DORA report page has the correct header.', async ({ page }) => {
   await expect(page.locator('h1')).toContainText('DORA Research: 2024');
-});
-
-test('2024 DORA report page has the correct language.', async ({ page }) => {
-  const languageOptions = await page.locator('item ul li').count();
-  await expect(languageOptions).toBe(1);
-
-  for (const language in languageToUrlMap) {
-    const url = languageToUrlMap[language];
-    const languageLink = page.getByRole('link', { name: language, exact: true });
-    await expect(languageLink).toHaveAttribute('href', url);
-  }
 });
 
 test('2024 DORA report page has the correct sidebar.', async ({ page }) => {
