8a8d10cc7d76f8d369816139288d86e3c3609061
Nathen Harvey <nathenharvey@google.com>
2024-10-18 21:41:46 -0400

    Add 2024 to `/publications/errata`
    Signed-off-by: Nathen Harvey <nathenharvey@google.com>

---
+++ test/playwright/tests/publications/errata.spec.ts
@@ -0,0 +1,29 @@
+import { test, expect } from '@playwright/test';
+
+// baseURL default is defined in playwright.config.ts
+
+const yearToURLMap = {
+    'Accelerate State of DevOps 2023': '/research/2023/errata/',
+    'Accelerate State of DevOps 2024': '/research/2024/errata/'
+};
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('/publications/errata/');
+});
+
+test('Publications errata has the correct title.', async ({ page }) => {
+  await expect(page).toHaveTitle('DORA | Errata');
+});
+
+test('Publications errata has the correct header.', async ({ page }) => {
+  await expect(page.locator('h1')).toContainText('DORA Publications Errata');
+});
+
+for (const year in yearToURLMap) {
+  const url = yearToURLMap[year];
+
+  test(`Publications errata links to the ${year} page.`, async ({ page }) => {
+    const yearLink = page.getByRole('link', { name: year, exact: true });
+    await expect(yearLink).toHaveAttribute('href', url);
+  });
+}
