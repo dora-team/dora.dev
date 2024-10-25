08e5c26240a6483204b2a2f5fbc9eaf891747c44
Nathen Harvey <nathenharvey@google.com>
2024-10-25 10:30:18 -0400

    Updates for v. 2024.2 of the report
    Signed-off-by: Nathen Harvey <nathenharvey@google.com>

--- test/playwright/tests/vc/version-check.spec.ts
+++ test/playwright/tests/vc/version-check.spec.ts
@@ -3,18 +3,32 @@ import { test, expect } from '@playwright/test';
 // No version specified
 test('Version checker has the correct title.', async ({ page }) => {
   await page.goto('/vc/');
+
   await expect(page).toHaveTitle('DORA | DORA Report Version Check');
 });
 
 // 2024.1 version
 test('Version checker has the correct header.', async ({ page }) => {
   await page.goto('/vc/?v=2024.1');
+
   await expect(page.locator('h1')).toContainText('DORA Report Version Check');
+});
+
+test('Version checker recognizes v 2024.2', async ({ page }) => {
+  await page.goto('/vc/?v=2024.2');
+
+  await expect(page.locator('h2:has-text("2024 DORA Report")').first()).toBeVisible();
+  await expect(page.locator('h2:has-text("2024 DORA Report")').last()).toBeHidden();
+  await expect(page.locator('h2:has-text("2023 DORA Report")').first()).toBeHidden();
+  await expect(page.locator('h2:has-text("2023 DORA Report")').last()).toBeHidden();
+  await expect(page.locator('h2', { hasText: 'Unrecognized version' })).toBeHidden();
 });
 
 test('Version checker recognizes v 2024.1', async ({ page }) => {
   await page.goto('/vc/?v=2024.1');
-  await expect(page.locator('h2:has-text("2024 DORA Report")').first()).toBeVisible();
+
+  await expect(page.locator('h2:has-text("2024 DORA Report")').first()).toBeHidden();
+  await expect(page.locator('h2:has-text("2024 DORA Report")').last()).toBeVisible();
   await expect(page.locator('h2:has-text("2023 DORA Report")').first()).toBeHidden();
   await expect(page.locator('h2:has-text("2023 DORA Report")').last()).toBeHidden();
   await expect(page.locator('h2', { hasText: 'Unrecognized version' })).toBeHidden();
@@ -22,7 +36,9 @@ test('Version checker recognizes v 2024.1', async ({ page }) => {
 
 test('Version checker recognizes v 2023-12', async ({ page }) => {
   await page.goto('/vc/?v=2023-12');
+
   await expect(page.locator('h2:has-text("2024 DORA Report")').first()).toBeHidden();
+  await expect(page.locator('h2:has-text("2024 DORA Report")').last()).toBeHidden();
   await expect(page.locator('h2:has-text("2023 DORA Report")').first()).toBeVisible();
   await expect(page.locator('h2:has-text("2023 DORA Report")').last()).toBeHidden();
   await expect(page.locator('h2', { hasText: 'Unrecognized version' })).toBeHidden();
@@ -30,7 +46,9 @@ test('Version checker recognizes v 2023-12', async ({ page }) => {
 
 test('Version checker recognizes v 2023-10', async ({ page }) => {
   await page.goto('/vc/?v=2023-10');
+
   await expect(page.locator('h2:has-text("2024 DORA Report")').first()).toBeHidden();
+  await expect(page.locator('h2:has-text("2024 DORA Report")').last()).toBeHidden();
   await expect(page.locator('h2:has-text("2023 DORA Report")').first()).toBeHidden();
   await expect(page.locator('h2:has-text("2023 DORA Report")').last()).toBeVisible();
   await expect(page.locator('h2', { hasText: 'Unrecognized version' })).toBeHidden();
@@ -38,7 +56,9 @@ test('Version checker recognizes v 2023-10', async ({ page }) => {
 
 test('Version checker handles empty version', async ({ page }) => {
   await page.goto('/vc/?v=');
+
   await expect(page.locator('h2:has-text("2024 DORA Report")').first()).toBeHidden();
+  await expect(page.locator('h2:has-text("2024 DORA Report")').last()).toBeHidden();
   await expect(page.locator('h2:has-text("2023 DORA Report")').first()).toBeHidden();
   await expect(page.locator('h2:has-text("2023 DORA Report")').last()).toBeHidden();
   await expect(page.locator('h2', { hasText: 'Unrecognized version' })).toBeVisible();
@@ -46,7 +66,9 @@ test('Version checker handles empty version', async ({ page }) => {
 
 test('Version checker handles no query', async ({ page }) => {
   await page.goto('/vc/');
+
   await expect(page.locator('h2:has-text("2024 DORA Report")').first()).toBeHidden();
+  await expect(page.locator('h2:has-text("2024 DORA Report")').last()).toBeHidden();
   await expect(page.locator('h2:has-text("2023 DORA Report")').first()).toBeHidden();
   await expect(page.locator('h2:has-text("2023 DORA Report")').last()).toBeHidden();
   await expect(page.locator('h2', { hasText: 'Unrecognized version' })).toBeVisible();
@@ -56,7 +78,9 @@ test('Version checker handles bogus version', async ({ page }) => {
   // Generate a random number between 1 and 1000
   const randomNumber = Math.floor(Math.random() * 1000) + 1;
   await page.goto('/vc/?v=' + randomNumber);
+
   await expect(page.locator('h2:has-text("2024 DORA Report")').first()).toBeHidden();
+  await expect(page.locator('h2:has-text("2024 DORA Report")').last()).toBeHidden();
   await expect(page.locator('h2:has-text("2023 DORA Report")').first()).toBeHidden();
   await expect(page.locator('h2:has-text("2023 DORA Report")').last()).toBeHidden();
   await expect(page.locator('h2', { hasText: 'Unrecognized version' })).toBeVisible();
@@ -67,17 +91,35 @@ test('Version checker handles bogus two part version', async ({ page }) => {
   const randomNumber = Math.floor(Math.random() * 1000) + 1;
   const randomNumber2 = Math.floor(Math.random() * 1000) + 1;
   await page.goto('/vc/?v=' + randomNumber + '-' + randomNumber2);
+
   await expect(page.locator('h2:has-text("2024 DORA Report")').first()).toBeHidden();
+  await expect(page.locator('h2:has-text("2024 DORA Report")').last()).toBeHidden();
   await expect(page.locator('h2:has-text("2023 DORA Report")').first()).toBeHidden();
   await expect(page.locator('h2:has-text("2023 DORA Report")').last()).toBeHidden();
   await expect(page.locator('h2', { hasText: 'Unrecognized version' })).toBeVisible();
 });
 
+test('Version checker handles bogus decimal version', async ({ page }) => {
+  // Generate a random number between 1 and 1000
+  const randomNumber = Math.floor(Math.random() * 1000) + 1;
+  const randomNumber2 = Math.floor(Math.random() * 1000) + 1;
+  await page.goto('/vc/?v=' + randomNumber + '.' + randomNumber2);
+
+  await expect(page.locator('h2:has-text("2024 DORA Report")').first()).toBeHidden();
+  await expect(page.locator('h2:has-text("2024 DORA Report")').last()).toBeHidden();
+  await expect(page.locator('h2:has-text("2023 DORA Report")').first()).toBeHidden();
+  await expect(page.locator('h2:has-text("2023 DORA Report")').last()).toBeHidden();
+  await expect(page.locator('h2', { hasText: 'Unrecognized version' })).toBeVisible();
+
+});
+
 test('Version checker handles extra data in the query string', async ({ page }) => {
   const randomNumber = Math.floor(Math.random() * 1000) + 1;
   const randomNumber2 = Math.floor(Math.random() * 1000) + 1;
-  await page.goto('/vc/?foo=' + randomNumber + '&bar=' + randomNumber2 + '&v=2024.1');
+  await page.goto('/vc/?foo=' + randomNumber + '&bar=' + randomNumber2 + '&v=2024.2');
+
   await expect(page.locator('h2:has-text("2024 DORA Report")').first()).toBeVisible();
+  await expect(page.locator('h2:has-text("2024 DORA Report")').last()).toBeHidden();
   await expect(page.locator('h2:has-text("2023 DORA Report")').first()).toBeHidden();
   await expect(page.locator('h2:has-text("2023 DORA Report")').last()).toBeHidden();
   await expect(page.locator('h2', { hasText: 'Unrecognized version' })).toBeHidden();
