7398937fc080ad4bfed3ae64e18cc211b0c11f39
Nathen Harvey <nathenharvey@google.com>
2025-01-31 13:05:18 -0500

    fix tests

--- test/playwright/tests/research/ai/value-of-development-work.spec.ts
+++ test/playwright/tests/research/ai/value-of-development-work.spec.ts
@@ -9,11 +9,11 @@ test('Value of development work has the correct title.', async ({ page }) => {
   await expect(page).toHaveTitle('DORA | How gen AI affects the value of development work');
 });
 
-test.('Value of development work has the correct header.', async ({ page }) => {
+test('Value of development work has the correct header.', async ({ page }) => {
   await expect(page.locator('h1 >> nth=1')).toContainText('How gen AI affects the value of development work');
 });
 
-test.('Value of development work has the correct sidebar.', async ({ page }) => {
+test('Value of development work has the correct sidebar.', async ({ page }) => {
   for (const sidebarLink of sidebarLinks) {
     await expect(page.getByRole('link', { name: sidebarLink, exact: true })).toBeVisible();
   }
