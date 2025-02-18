836fd443ba96e455b3d9139b708a9a8140553563
Nathen Harvey <nathenharvey@google.com>
2025-02-18 10:31:45 -0500

    T-shirt sales have ended
    * Removes feature from the home page
    * Remove links to the order forms
    * Update messaging on the page
    * Update tests

--- test/playwright/tests/merch/merch.spec.ts
+++ test/playwright/tests/merch/merch.spec.ts
@@ -11,7 +11,7 @@ test('Merch page has the correct title.', async ({ page }) => {
 });
 
 test('Merch page has the correct header.', async ({ page }) => {
-  await expect(page.locator('h1')).toContainText('DORA Merchandise');
+  await expect(page.locator('h1').first()).toContainText('DORA Merchandise');
 });
 
 test('Merch page has email for merchandise information.', async ({ page }) => {
