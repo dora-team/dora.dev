572cf261c0f3f2f7f300b8048036d1b1881af809
Nathen Harvey <nathenharvey@google.com>
2025-03-11 19:47:40 -0400

    2024 report is avaialable in multiple languages
    * Adds language-specific links to the report page
    * Removes the "download the report" button
    * Updates tests

--- test/playwright/tests/research/2024/2024-dora-report.spec.ts
+++ test/playwright/tests/research/2024/2024-dora-report.spec.ts
@@ -1,6 +1,18 @@
 import { test, expect } from '@playwright/test';
 import { sidebarLinks } from '../sidebarLinks';
 
+const languageToUrlMap = {
+  'English': 'https://cloud.google.com/devops/state-of-devops/?hl=en&region=US',
+  'Español': 'https://cloud.google.com/devops/state-of-devops/?hl=es&region=ES',
+  'Español - América Latina': 'https://cloud.google.com/devops/state-of-devops/?hl=es-419&region=MX',
+  'Français': 'https://cloud.google.com/devops/state-of-devops/?hl=fr&region=FR',
+  'Italiano': 'https://cloud.google.com/devops/state-of-devops/?hl=it&region=IT',
+  'Português - Brasil': 'https://cloud.google.com/devops/state-of-devops/?hl=pt-br&region=BR',
+  '中文 – 简体': 'https://cloud.google.com/devops/state-of-devops/?hl=zh-tw&region=TW' ,
+  '日本語': 'https://cloud.google.com/devops/state-of-devops/?hl=ja&region=JP',
+  '한국어': 'https://cloud.google.com/devops/state-of-devops/?hl=ko&region=KR'
+};
+
 test.beforeEach(async ({ page }) => {
   await page.goto('/research/2024/dora-report/');
 });
@@ -12,6 +24,21 @@ test('2024 research overview has the correct title.', async ({ page }) => {
 test('2024 research overview has the correct header.', async ({ page }) => {
   await expect(page.locator('h1')).toContainText('DORA Research: 2024');
 });
+
+test('2024 DORA report page has the correct number of language options.', async ({ page }) => {
+  const languageOptions = await page.locator('item ul li').count();
+  const expectedLanguageCount = Object.keys(languageToUrlMap).length;
+  await expect(languageOptions).toBe(expectedLanguageCount);
+});
+
+for (const language in languageToUrlMap) {
+  const url = languageToUrlMap[language];
+  test(`2024 DORA report is available in ${language}`, async ({ page }) => {
+    const url = languageToUrlMap[language];
+    const languageLink = page.getByRole('link', { name: language, exact: true });
+    await expect(languageLink).toHaveAttribute('href', url);
+  });
+}
 
 test('2024 research overview has the correct sidebar.', async ({ page }) => {
   for (const sidebarLink of sidebarLinks) {
