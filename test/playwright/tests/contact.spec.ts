67b3d4e9614d26ee2f8a088b03384acbf33c3300
Nathen Harvey <nathenharvey@google.com>
2025-03-25 15:20:56 -0400

    Refactor tests for the contact us page

--- test/playwright/tests/contact.spec.ts
+++ test/playwright/tests/contact.spec.ts
@@ -1,7 +1,34 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
+test.beforeEach(async ({ page }) => {
   await page.goto('/contact/');
-  await expect(page.locator('#contact-dora')).toContainText('Contact DORA');
-  await expect(page.getByRole('main')).toContainText('dora-advocacy@google.com');
 });
+
+test('Contact page has the correct title.', async ({ page }) => {
+  await expect(page).toHaveTitle('DORA | Contact Us');
+});
+
+test('Contact page has the correct header.', async ({ page }) => {
+  await expect(page.locator('h1')).toContainText('Contact DORA');
+});
+
+const emailAddresses = [
+  'dora-advocacy@google.com',
+  'sponsor-dora@google.com',
+];
+
+for (const emailAddress of emailAddresses) {
+  test(`Contact page has mail to link for ${emailAddress}.`, async ({ page }) => {
+    // Find the firsst link that contains the email address
+    const emailLink = page.getByRole('link', { name: emailAddress }).first();
+
+    // Check if the link is visible
+    await expect(emailLink).toBeVisible();
+
+    // Check if the link starts with the correct href attribute.
+    // This accomodates a query string, like a subject, in the link
+    const href = await emailLink.getAttribute('href');
+    expect(href).not.toBeNull();
+    expect(href?.startsWith(`mailto:${emailAddress}`)).toBe(true);
+  });
+}
