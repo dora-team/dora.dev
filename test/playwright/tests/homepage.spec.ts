07e96a7f1af02746e2e0286e798035ed9da8ab23
Dave Stanke <davidstanke@gmail.com>
2025-04-04 11:40:54 -0400

    Update homepage tests (fixes #978)

--- test/playwright/tests/homepage.spec.ts
+++ test/playwright/tests/homepage.spec.ts
@@ -2,49 +2,30 @@ import { test, expect } from '@playwright/test';
 
 // baseURL default is defined in playwright.config.ts
 
-export const heroImageToUrlMap = {
-  '/research/ai/gen-ai-report/dora-impact-of-generative-ai-in-software-development-report.png': 'https://cloud.google.com/resources/content/dora-impact-of-gen-ai-software-development',
-  '/img/quickcheck/hero_illustration.svg': '/quickcheck/',
-  '/research/2024/dora-report-ai-preview-hero.png': '/research/ai/',
-  '/img/features/google-cloud-next.png': 'https://cloud.withgoogle.com/next/25?utm_source=gamma&utm_medium=email&utm_campaign=FY25-Q2-global-EXP106-physicalevent-er-next25-mc&utm_content=dora-community-letter&utm_term=-',
-  '/img/features/homepage-core-snipe.png': '/research/'
-};
-
 test.beforeEach(async ({ page }) => {
   await page.goto('/');
-});
-
-test('test', async ({ page }) => {
-  await page.goto('/');
-  await expect(page.getByText('Get Better at Getting Better')).toBeVisible();
 });
 
 test('Homepage has the correct title.', async ({ page }) => {
   await expect(page).toHaveTitle('DORA | Get Better at Getting Better');
 });
 
-test('Homepage has the correct header.', async ({ page }) => {
-  await expect(page.locator('h1')).toContainText('Get Better at Getting Better');
+test('Homepage has the correct lede.', async ({ page }) => {
+  await expect(page.locator('h1').first()).toContainText('Get better at getting better');
 });
 
-for (const imageSrc in heroImageToUrlMap) {
-  const imageName = imageSrc.substring(imageSrc.lastIndexOf('/') + 1, imageSrc.lastIndexOf('.'));
-  const expectedUrl = heroImageToUrlMap[imageSrc];
-  test(`Homepage hero image ${imageName} links to ${expectedUrl}`, async ({ page }) => {
-    // Find the parent element (likely an <a> tag) that contains the image
-    const imageContainer = page.locator(`img[src="${imageSrc}"]`).locator('xpath=..');
+test('Hero report link is visible.', async ({ page }) => {
+await expect(page.getByRole('button', { name: 'Read our latest report' })).toBeVisible();
+});
 
-    // Check if the parent element is a link (<a> tag)
-    const isLink = await imageContainer.evaluate((el) => el.tagName.toLowerCase() === 'a');
+test('Feature section ("our latest report") is visible.', async ({ page }) => {
+await expect(page.getByRole('heading', { name: 'our latest report' })).toBeVisible();
+});
 
-    if (isLink) {
-      // If it's a link, get the href attribute
-      const actualUrl = await imageContainer.getAttribute('href');
-      // Assert that the href attribute matches the expected URL
-      await expect(actualUrl).toBe(expectedUrl);
-    } else {
-      // If it's not a link, fail the test
-      throw new Error(`Image ${imageName} is not wrapped in a link.`);
-    }
-  });
-}
+test('Resources header is visible.', async ({ page }) => {
+await expect(page.getByRole('heading', { name: 'Resources' })).toBeVisible();
+});
+
+test('There are four snipes under "Resources."', async ({ page }) => {
+  await expect(page.locator('.homepage-snipe')).toHaveCount(4);
+});
