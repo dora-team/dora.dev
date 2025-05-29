0b94297286495804ebcd0343fa56b949a6105958
Nathen Harvey <nathenharvey@google.com>
2025-05-29 09:43:20 -0400

    Update /survey in preparation for 2025

--- test/playwright/tests/survey/index.spec.ts
+++ test/playwright/tests/survey/index.spec.ts
@@ -2,10 +2,10 @@ const { test, expect } = require('@playwright/test');
 
 test('Verify the page title', async ({ page }) => {
   await page.goto('/survey/');
-  await expect(page).toHaveTitle('DORA | The 2024 DORA Survey');
+  await expect(page).toHaveTitle('DORA | The 2025 DORA Survey');
 });
 
-test('The 2024 survey is closed', async ({ page }) => {
+test('The 2025 survey is coming soon', async ({ page }) => {
   await page.goto('/survey/');
-  await expect(page.locator('h1')).toContainText('The 2024 DORA Survey is now closed');
+  await expect(page.locator('h1')).toContainText('The 2025 DORA Survey is coming soon');
 });
