b28b00907ac9b25e2947f827bc48d4dc9eb9a55d
Nathen Harvey <nathenharvey@google.com>
2025-07-07 12:10:22 -0400

    A better playwright test for last updated dates
    Now looks for a date string, using a regex, following the "Last updated: " string.

--- test/playwright/tests/decade/decade-with-dora.spec.ts
+++ test/playwright/tests/decade/decade-with-dora.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from '@playwright/test';
+import { LAST_UPDATED_DATE_REGEX } from '../constants';
 
 test.beforeEach(async ({ page }) => {
   await page.goto('/decade');
@@ -13,5 +14,5 @@ test('Decade with DORA has the correct header.', async ({ page }) => {
 });
 
 test('Decade with DORA displays its last updated date', async ({ page }) => {
-  await expect(page.locator('.updated')).toContainText('Last updated: ')
+  await expect(page.locator('.updated')).toContainText(LAST_UPDATED_DATE_REGEX);
 });
