4b445d6169a234316e54d8c2b52963ebe30994af
Nathen Harvey <nathenharvey@google.com>
2025-08-12 09:02:30 -0400

    refactor(tests): group playwright tests for the home page

--- test/playwright/tests/homepage.spec.ts
+++ test/playwright/tests/homepage.spec.ts
@@ -1,31 +1,32 @@
 import { test, expect } from '@playwright/test';
 
 // baseURL default is defined in playwright.config.ts
+test.describe('Homepage', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/');
+  });
 
-test.beforeEach(async ({ page }) => {
-  await page.goto('/');
-});
+  test('has the correct title.', async ({ page }) => {
+    await expect(page).toHaveTitle('DORA | Get Better at Getting Better');
+  });
 
-test('Homepage has the correct title.', async ({ page }) => {
-  await expect(page).toHaveTitle('DORA | Get Better at Getting Better');
-});
+  test('has the correct lede.', async ({ page }) => {
+    await expect(page.locator('h1').first()).toContainText('Get better at getting better');
+  });
 
-test('Homepage has the correct lede.', async ({ page }) => {
-  await expect(page.locator('h1').first()).toContainText('Get better at getting better');
-});
+  test('hero report link is visible.', async ({ page }) => {
+  await expect(page.getByRole('button', { name: 'Read our latest report' })).toBeVisible();
+  });
 
-test('Hero report link is visible.', async ({ page }) => {
-await expect(page.getByRole('button', { name: 'Read our latest report' })).toBeVisible();
-});
+  test('feature section ("our latest report") is visible.', async ({ page }) => {
+  await expect(page.getByRole('heading', { name: 'our latest report' })).toBeVisible();
+  });
 
-test('Feature section ("our latest report") is visible.', async ({ page }) => {
-await expect(page.getByRole('heading', { name: 'our latest report' })).toBeVisible();
-});
+  test('resources header is visible.', async ({ page }) => {
+  await expect(page.getByRole('heading', { name: 'Resources' })).toBeVisible();
+  });
 
-test('Resources header is visible.', async ({ page }) => {
-await expect(page.getByRole('heading', { name: 'Resources' })).toBeVisible();
-});
-
-test('There are four snipes under "Resources."', async ({ page }) => {
-  await expect(page.locator('.homepage-snipe')).toHaveCount(4);
+  test('there are four snipes under "Resources."', async ({ page }) => {
+    await expect(page.locator('.homepage-snipe')).toHaveCount(4);
+  });
 });
