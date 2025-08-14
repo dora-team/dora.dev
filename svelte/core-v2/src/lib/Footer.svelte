85031630c9feb7c30a77bb7e1d953f78c1bd57e4
Dave Stanke <davidstanke@google.com>
2025-08-14 18:26:59 -0400

    update core language Fixes #1047

--- svelte/core-v2/src/lib/Footer.svelte
+++ svelte/core-v2/src/lib/Footer.svelte
@@ -1,28 +1,28 @@
 <script>
     export let view_mode = "summary";
-    import png_url_summary from '../assets/png/dora-core-v2.0.0-summary.png'
-    import png_url_detail from '../assets/png/dora-core-v2.0.0-detail.png'
-    import pdf_url_summary from '../assets/pdf/dora-core-v2.0.0-summary.pdf'
-    import pdf_url_detail from '../assets/pdf/dora-core-v2.0.0-detail.pdf'
+    import png_url_summary from "../assets/png/dora-core-v2.1.0-summary.png";
+    import png_url_detail from "../assets/png/dora-core-v2.1.0-detail.png";
+    import pdf_url_summary from "../assets/pdf/dora-core-v2.1.0-summary.pdf";
+    import pdf_url_detail from "../assets/pdf/dora-core-v2.1.0-detail.pdf";
 
     let download_urls = {
-        "png": {
-        "summary": png_url_summary,
-        "detail": png_url_detail
+        png: {
+            summary: png_url_summary,
+            detail: png_url_detail,
         },
-        "pdf": {
-        "summary": pdf_url_summary,
-        "detail": pdf_url_detail
+        pdf: {
+            summary: pdf_url_summary,
+            detail: pdf_url_detail,
         },
-    }
+    };
 </script>
 
 <div class="core-footer">
-    <div class="version">DORA Core model v2.0.0</div>
+    <div class="version">DORA Core model v2.1.0</div>
     <div>
-        download: 
-        <a href="{download_urls['png'][view_mode]}" target="_blank">PNG</a>
-        <a href="{download_urls['pdf'][view_mode]}" target="_blank">PDF</a>
+        download:
+        <a href={download_urls["png"][view_mode]} target="_blank">PNG</a>
+        <a href={download_urls["pdf"][view_mode]} target="_blank">PDF</a>
     </div>
 </div>
 
@@ -33,12 +33,12 @@
         margin-bottom: 2rem;
         font-size: 0.75rem;
         column-gap: 1rem;
-        color:var(--color-grey-medium)
+        color: var(--color-grey-medium);
     }
 
     a {
-        color:var(--color-grey-medium);
-        display:inline-block;
-        margin-inline: .15rem;
+        color: var(--color-grey-medium);
+        display: inline-block;
+        margin-inline: 0.15rem;
     }
 </style>
