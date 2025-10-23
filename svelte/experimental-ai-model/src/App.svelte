db5112344ca84e44630e1c141c20539c60f82cde
Dave Stanke <davidstanke@google.com>
2025-10-23 19:22:21 -0400

    add connectors

--- svelte/experimental-ai-model/src/App.svelte
+++ svelte/experimental-ai-model/src/App.svelte
@@ -1,5 +1,6 @@
-<script lang="ts">
+<script>
   import data from "./data.json";
+  import Connector from "./lib/Connector.svelte";
   const { capabilities, outcomes } = data;
 </script>
 
@@ -16,7 +17,10 @@
         <div class="entity" id={capability.id}>{capability.name}</div>
       {/each}
     </div>
-    <div class="connectors">...</div>
+    <div class="connectors">
+      <Connector fromId="user-centric-focus" toId="team-performance" index="0"/>
+      <Connector fromId="strong-version-control-practices" toId="team-performance" index="1" />
+    </div>
     <div class="outcomes">
       {#each outcomes as outcome}
         <div class="entity" id={outcome.id}>{outcome.name}</div>
@@ -38,12 +42,21 @@
     justify-content: center;
     align-items: center;
     column-gap: 12px;
+    background-color: white;
 
     .entity {
       border: 1px solid var(--dora-primary-dark);
       border-radius: 4px;
       padding: 4px 12px;
       margin: 12px;
+      /* text-wrap: nowrap; */
+    }
+
+    .connectors {
+      width: 200px;
+      padding-right:24px;
+      position: relative;
+      align-self: stretch;
     }
 
     .outcomes .entity {
