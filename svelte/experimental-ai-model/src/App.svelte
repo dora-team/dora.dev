5346aa7553b8ac2e581506185b354cde8f58a350
Dave Stanke <davidstanke@google.com>
2025-10-24 11:41:48 -0400

    add all arrows

--- svelte/experimental-ai-model/src/App.svelte
+++ svelte/experimental-ai-model/src/App.svelte
@@ -10,7 +10,7 @@
       <div class="entity">AI Adoption</div>
     </div>
     <div class="x">
-      <div class="entity">x</div>
+      <div class="entity">&times;</div>
     </div>
     <div class="capabilities">
       {#each capabilities as capability}
@@ -20,6 +20,20 @@
     <div class="connectors">
       <Connector fromId="user-centric-focus" toId="team-performance" index="0"/>
       <Connector fromId="strong-version-control-practices" toId="team-performance" index="1" />
+      <Connector fromId="strong-version-control-practices" toId="individual-effectiveness" index="-1" />
+      <Connector fromId="ai-accessible-internal-data" toId="code-quality" index="0" />
+      <Connector fromId="ai-accessible-internal-data" toId="individual-effectiveness" index="0" />
+      <Connector fromId="ai-accessible-internal-data" toId="product-performance" index="-1" />
+      <Connector fromId="working-in-small-batches" toId="individual-effectiveness" index="1" />
+      <Connector fromId="working-in-small-batches" toId="product-performance" index="0" />
+      <Connector fromId="working-in-small-batches" toId="friction" index="-1" />
+      <Connector fromId="clear-communicated-ai-stance" toId="individual-effectiveness" index="2" />
+      <Connector fromId="clear-communicated-ai-stance" toId="product-performance" index="1" />
+      <Connector fromId="clear-communicated-ai-stance" toId="throughput" index="0" />
+      <Connector fromId="clear-communicated-ai-stance" toId="organizational-performance" index="-2" />
+      <Connector fromId="quality-internal-platform" toId="friction" index="0" />
+      <Connector fromId="quality-internal-platform" toId="organizational-performance" index="-1" />
+      <Connector fromId="healthy-data-ecosystems" toId="organizational-performance" index="0" />
     </div>
     <div class="outcomes">
       {#each outcomes as outcome}
@@ -41,7 +55,7 @@
     flex-direction: row;
     justify-content: center;
     align-items: center;
-    column-gap: 12px;
+    /* column-gap: 12px; */
     background-color: white;
 
     .entity {
@@ -49,12 +63,14 @@
       border-radius: 4px;
       padding: 4px 12px;
       margin: 12px;
-      /* text-wrap: nowrap; */
+      font-size:14px;
+      font-weight: 400;
+      text-wrap: nowrap;
     }
 
     .connectors {
-      width: 200px;
-      padding-right:24px;
+      width: 120px;
+      padding-right:4px;
       position: relative;
       align-self: stretch;
     }
