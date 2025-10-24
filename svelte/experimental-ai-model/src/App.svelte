b2d5598b2755eb4ec9fe6785ec88405a0fa2aa78
Dave Stanke <davidstanke@google.com>
2025-10-24 12:06:36 -0400

    dim outcomes

--- svelte/experimental-ai-model/src/App.svelte
+++ svelte/experimental-ai-model/src/App.svelte
@@ -5,6 +5,36 @@
   import Outcome from "./lib/Outcome.svelte";
   const { capabilities, outcomes } = data;
   let hoveredCapabilityId = null;
+
+  const connections = [
+    { from: "user-centric-focus", to: "team-performance" },
+    { from: "strong-version-control-practices", to: "team-performance" },
+    { from: "strong-version-control-practices", to: "individual-effectiveness" },
+    { from: "ai-accessible-internal-data", to: "code-quality" },
+    { from: "ai-accessible-internal-data", to: "individual-effectiveness" },
+    { from: "ai-accessible-internal-data", to: "product-performance" },
+    { from: "working-in-small-batches", to: "individual-effectiveness" },
+    { from: "working-in-small-batches", to: "product-performance" },
+    { from: "working-in-small-batches", to: "friction" },
+    { from: "clear-communicated-ai-stance", to: "individual-effectiveness" },
+    { from: "clear-communicated-ai-stance", to: "product-performance" },
+    { from: "clear-communicated-ai-stance", to: "throughput" },
+    { from: "clear-communicated-ai-stance", to: "organizational-performance" },
+    { from: "quality-internal-platform", to: "friction" },
+    { from: "quality-internal-platform", to: "organizational-performance" },
+    { from: "healthy-data-ecosystems", to: "organizational-performance" },
+  ];
+
+  let connectedOutcomeIds = [];
+  $: {
+    if (hoveredCapabilityId) {
+      connectedOutcomeIds = connections
+        .filter((c) => c.from === hoveredCapabilityId)
+        .map((c) => c.to);
+    } else {
+      connectedOutcomeIds = [];
+    }
+  }
 </script>
 
 <main>
@@ -49,7 +79,7 @@
     </div>
     <div class="outcomes">
       {#each outcomes as outcome}
-        <Outcome {outcome} />
+        <Outcome {outcome} dimmed={hoveredCapabilityId && !connectedOutcomeIds.includes(outcome.id)} />
       {/each}
     </div>
   </div>
--- svelte/experimental-ai-model/src/lib/Outcome.svelte
+++ svelte/experimental-ai-model/src/lib/Outcome.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
   export let outcome;
+  export let dimmed = false;
 </script>
 
-<div class="entity" id={outcome.id}>{outcome.name}</div>
+<div class="entity" class:dimmed id={outcome.id}>{outcome.name}</div>
 
 <style>
   .entity {
     background-color:var(--dora-highlight);
+  }
+  .dimmed {
+    opacity: 0.5;
+    background-color:transparent;
   }
 </style>
