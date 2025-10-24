0b32e4719a14d5f962cc44ba55d1513dcbcbaa11
Dave Stanke <davidstanke@google.com>
2025-10-24 13:07:25 -0400

    svelte5-ify

--- svelte/experimental-ai-model/src/lib/Capability.svelte
+++ svelte/experimental-ai-model/src/lib/Capability.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
-  export let capability;
-  export let dimmed = false;
-  export let hovered = false;
+  let { capability, dimmed = false, hovered = false } = $props();
 </script>
 
 <div class="entity" class:dimmed class:hovered id={capability.id}>{capability.name}</div>
