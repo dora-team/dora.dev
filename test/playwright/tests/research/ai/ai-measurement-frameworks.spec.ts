04b0fe0a544ba096d65716d2ef3e4f76cc606d0f
Nathen Harvey <nathenharvey@google.com>
2025-10-24 14:52:54 -0400

    Update tests for the AI measurement framework page
    This page is generated through a shared content bundle. Instead of two nearly identical tests, consolidate into one test file and test both paths.

--- test/playwright/tests/research/ai/ai-measurement-frameworks.spec.ts
+++ test/playwright/tests/research/ai/ai-measurement-frameworks.spec.ts
@@ -1,20 +1,43 @@
 import { test, expect } from '@playwright/test';
 import { sidebarLinks } from '../sidebarLinks';
 
-test.describe('AI measurement frameworks page', () => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto('/research/ai/measurement-frameworks/');
-  });
+const testCases = [
+  {
+    url: '/research/2025/measurement-frameworks/',
+    header: 'DORA Research: 2025',
+    name: '2025',
+  },
+  {
+    url: '/research/ai/measurement-frameworks/',
+    header: 'DORA Research: Artificial Intelligence',
+    name: 'AI',
+  },
+];
 
-  test('has the correct title.', async ({ page }) => {
-    await expect(page).toHaveTitle('DORA | Choosing measurement frameworks to fit your organizational goals');
-  });
+for (const { url, header, name } of testCases) {
+  test.describe(`${name} measurement frameworks page`, () => {
+    test.beforeEach(async ({ page }) => {
+      await page.goto(url);
+    });
 
-  test('has the correct header.', async ({ page }) => {
-    await expect(page.locator('h1').first()).toContainText('DORA Research: Artificial Intelligence');
-  });
+    test('has the correct title.', async ({ page }) => {
+      await expect(page).toHaveTitle(
+        'DORA | Choosing measurement frameworks to fit your organizational goals'
+      );
+    });
 
-  test('has the correct sidebar.', async ({ page }) => {
-    await Promise.all(sidebarLinks.map((sidebarLink) => expect(page.getByRole('link', { name: sidebarLink, exact: true })).toBeVisible()));
+    test('has the correct header.', async ({ page }) => {
+      await expect(page.locator('h1').first()).toContainText(header);
+    });
+
+    test('has the correct sidebar.', async ({ page }) => {
+      await Promise.all(
+        sidebarLinks.map((sidebarLink) =>
+          expect(
+            page.getByRole('link', { name: sidebarLink, exact: true })
+          ).toBeVisible()
+        )
+      );
+    });
   });
-});
+}
