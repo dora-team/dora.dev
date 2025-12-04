53e3d4e26149904b7ba0f5e29965ace9fafb0f2b
Dave Stanke <davidstanke@google.com>
2025-12-04 10:09:42 -0500

    move files around

---
+++ svelte/ai-model/build-ai-model.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+# run this script from the project root
+cd svelte/ai-model
+
+# build production artifacts
+npm install
+npm run build
+
+# copy assets to the quickcheck content folder
+mkdir -p ../../hugo/content/ai/capabilities-model/interactive
+cp dist/assets/index-*.js ../../hugo/content/ai/capabilities-model/interactive/ai-model.js
+cp dist/assets/index-*.css ../../hugo/content/ai/capabilities-model/interactive/ai-model.css
