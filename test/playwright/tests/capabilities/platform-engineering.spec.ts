659ff2da5491866980e9b5bd4b127d2ec978a30f
Nathen Harvey <nathenharvey@google.com>
2025-12-08 08:24:36 -0500

    Add tests for platform engineering capability

---
+++ test/playwright/tests/capabilities/platform-engineering.spec.ts
@@ -0,0 +1,36 @@
+import { test, expect } from "@playwright/test";
+import { LAST_UPDATED_DATE_REGEX } from "../constants";
+
+test.describe("Platform engineering capability", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("/capabilities/platform-engineering/");
+  });
+
+  test("has the correct title", async ({ page }) => {
+    await expect(page).toHaveTitle("DORA | Platform Engineering");
+  });
+
+  test("has the correct header", async ({ page }) => {
+    await expect(page.locator("h1")).toHaveText("Platform Engineering");
+  });
+
+  test("displays its last updated date", async ({ page }) => {
+    await expect(page.locator(".updated")).toContainText(
+      LAST_UPDATED_DATE_REGEX,
+    );
+  });
+
+  test("is an AI capability", async ({ page }) => {
+    await expect(page.locator(".labels")).toContainText("AI");
+    await expect(page.getByRole('link', { name: 'AI', exact: true })).toBeVisible();
+  });
+
+  test("has the correct categories", async ({ page }) => {
+    const categories = [
+      "AI"
+    ];
+    for (const category of categories) {
+      await expect(page.locator(".categories")).toContainText(category);
+    }
+  });
+});
