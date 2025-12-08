e2a3fc9b1cbef9268bc50a7e0235ee2bf9576b27
Dave Stanke <davidstanke@google.com>
2025-12-08 16:37:56 -0500

    add purge script

---
+++ ci/purge-firebase-previews.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# This script will purge ALL firebase preview channels. (=all channels except `live`)
+# It's not very discriminating, so use with caution 
+# Before running it, run `firebase login`
+
+firebase hosting:channel:list --site doradotdev --json | jq -r '.result.channels[] | select((.name | split("/")[-1]) != "live") | .name | split("/")[-1]'
