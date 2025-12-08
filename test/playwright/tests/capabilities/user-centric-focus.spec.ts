868abf512263451d873c147493e1208c3baa90cd
Nathen Harvey <nathenharvey@google.com>
2025-12-08 10:46:20 -0500

    Add temporary User-centric Focus capability

---
+++ test/playwright/tests/capabilities/user-centric-focus.spec.ts
@@ -0,0 +1,36 @@
+import { test, expect } from "@playwright/test";
+import { LAST_UPDATED_DATE_REGEX } from "../constants";
+
+test.describe("User-centric Focus capability", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("/capabilities/user-centric-focus/");
+  });
+
+  test("has the correct title", async ({ page }) => {
+    await expect(page).toHaveTitle("DORA | User-centric focus");
+  });
+
+  test("has the correct header", async ({ page }) => {
+    await expect(page.locator("h1")).toContainText("User-centric focus");
+  });
+
+  test("displays its last updated date", async ({ page }) => {
+    await expect(page.locator(".updated")).toContainText(
+      LAST_UPDATED_DATE_REGEX,
+    );
+  });
+
+  test("is an AI capability", async ({ page }) => {
+    await expect(page.locator(".labels")).toContainText("AI");
+    await expect(page.getByRole('link', { name: 'AI', exact: true })).toBeVisible();
+  });
+
+  test("has the correct categories", async ({ page }) => {
+    const categories = [
+      "AI"
+    ];
+    for (const category of categories) {
+      await expect(page.locator(".categories")).toContainText(category);
+    }
+  });
+});
