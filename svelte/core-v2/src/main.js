d231ac906f37972e8c1552a425776ec265de06ef
Dave Stanke <davidstanke@google.com>
2025-12-22 16:13:36 -0500

    fix svelte issue

--- svelte/core-v2/src/main.js
+++ svelte/core-v2/src/main.js
@@ -2,6 +2,6 @@ import { mount } from 'svelte';
 import './app.css'
 import App from './App.svelte'
 
-const app = mount(App, { target: document.getElementById("core-model") });
+const app = mount(App, { target: document.getElementById("app") });
 
 export default app
