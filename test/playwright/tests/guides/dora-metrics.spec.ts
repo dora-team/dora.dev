8f56aa763185805619a1e71c2aa448fed9f443d8
Nathen Harvey <nathenharvey@google.com>
2026-03-11 12:34:39 -0400

    Migrate history of software delivery metrics to an Insight
    * Adds a redirect
    * Updates links (from the metrics guide)
    
    Fixes #1294

--- test/playwright/tests/guides/dora-metrics.spec.ts
+++ test/playwright/tests/guides/dora-metrics.spec.ts
@@ -29,23 +29,3 @@ test.describe("DORA metrics guide", () => {
         expect(box!.width).toBeGreaterThan(0);
     });
 });
-
-test.describe("DORA metrics history", () => {
-    test.beforeEach(async ({ page }) => {
-        await page.goto("/guides/dora-metrics/history/");
-    });
-
-    test("has the correct title", async ({ page }) => {
-        await expect(page).toHaveTitle('DORA | A history of DORA’s software delivery metrics');
-    });
-
-    test("has the correct header", async ({ page }) => {
-        await expect(page.locator('h1')).toContainText('A history of DORA’s software delivery metrics');
-    });
-
-    test("displays its last updated date", async ({ page }) => {
-        await expect(page.locator(".updated")).toContainText(
-            LAST_UPDATED_DATE_REGEX,
-        );
-    });
-});
