5c3c83c4292db45969fbc0cf3e1dca5e3637713b
Nathen Harvey <nathenharvey@google.com>
2026-03-12 09:02:10 -0400

    Add ROI calculator to svelte build script

--- svelte/build.sh
+++ svelte/build.sh
@@ -4,9 +4,11 @@ set -euo pipefail
 # This script will trigger builds of each of the svelte applications, in parallel.
 # RUN THIS SCRIPT FROM THE REPOSITORY ROOT
 
-./svelte/ai-model/build-ai-model.sh &
-./svelte/core-v2/build-core.sh &
-./svelte/quick-check/build-quick-check.sh &
+# doing roi steps manually to test the build in CI
+cd svelte/roi-calculator && npm install &
+# ./svelte/ai-model/build-ai-model.sh &
+# ./svelte/core-v2/build-core.sh &
+# ./svelte/quick-check/build-quick-check.sh &
 
 for job in $(jobs -p); do
   wait "$job"
