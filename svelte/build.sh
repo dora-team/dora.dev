da25747635e8a682de76dddd892bc47b252cdc5a
Nathen Harvey <nathenharvey@google.com>
2026-03-12 18:07:28 -0400

    Revert github workflow changes
    These were inadvertently added while building the roi calculator

--- svelte/build.sh
+++ svelte/build.sh
@@ -4,7 +4,6 @@ set -euo pipefail
 # This script will trigger builds of each of the svelte applications, in parallel.
 # RUN THIS SCRIPT FROM THE REPOSITORY ROOT
 
-# doing roi steps manually to test the build in CI
 ./svelte/roi-calculator/build-roi-calculator.sh &
 ./svelte/ai-model/build-ai-model.sh &
 ./svelte/core-v2/build-core.sh &
